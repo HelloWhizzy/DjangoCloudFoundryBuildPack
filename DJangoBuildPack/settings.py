cc3344ab77617881b658655c17bb00a4b0a6fd5b
gmanigandan <manigandan_g@yahoo.com>
2016-06-30 21:15:56 -0400

    Add files via upload

--- DJangoBuildPack/settings.py
+++ DJangoBuildPack/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'DJangoBuildPack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '<your db id - get it from cf env appname command>',
+        'USER': '<your user id - get it from cf env appname command>',
+        'PASSWORD': '<your password - get it from cf env appname command>',
+        'HOST': '<your hostname - get it from cf env appname command>',
+        'PORT': '5432',
     }
 }
 
